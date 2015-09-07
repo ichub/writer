f31cf2d52a82e51a5215a4a360283175af01a4e7
Ivan Chub <ivansergchub@gmail.com>
2015-09-07 15:03:10 -0700

    added typescript compilation

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('default', function() {
+  return gulp.src('src/*.ts')
+    .pipe(ts({
+    }))
+    .pipe(gulp.dest('dist/'));
+});
