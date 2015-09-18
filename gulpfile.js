3a612e51212f2c748e6d75692dc1e231f6fc2c55
Ivan Chub <ivansergchub@gmail.com>
2015-09-17 20:25:02 -0700

    use gulp for building

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var start = require('gulp-start-process');
 
-gulp.task('default', function() {
-  return gulp.src('src/*.ts')
+gulp.task('build', function() {
+	return gulp.src('src/*.ts')
     .pipe(ts({
     }))
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('run', ['build'], function(cb) {
+	return start('(node dist/writer.js example.hbs meta.json; open compiled.pdf)', cb);
+});
+
+gulp.task('default', ['run'], function() {
+
 });
